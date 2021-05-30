83db024878a6d53022c1b2bfeceb23a528d0aa20
Dwog <philliptdavies@gmail.com>
2021-05-31 02:32:48 +1200

    Motor 2

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,6 +10,7 @@ brain  Brain;
 // VEXcode device constructors
 controller Controller1 = controller(primary);
 motor Motor1 = motor(PORT1, ratio18_1, false);
+motor Motor2 = motor(Port2, ratio18_1, false);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
